a9650556723ae85b867fb76bd7cfda340f27dcda
ferrariflaviaa <flaviaferrarit@gmail.com>
2022-01-01 19:15:07 -0300

    First class: Position

--- src/applicatiton/Program.java
+++ src/applicatiton/Program.java
@@ -4,7 +4,9 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello Word");
+		Position pos = new Position (3, 5);
+		
+		System.out.println(pos);
 
 	}
 
